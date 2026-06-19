7cc2449fcb107089ffce3d370648f94fe0f08f1c
dasharpest <dasharpest@tutanota.com>
2026-06-20 00:40:28 +0100

    update fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,75 +1,79 @@
-cat ~/.cache/wal/sequences
-
-### SETTING THE STARSHIP PROMPT ###
-
-starship init fish | source
-
-if status is-interactive
-    # Commands to run in interactive sessions can go here
-end
-
-### ADDING TO THE PATH ###
-
+### ADDING TO THE PATH
 # First line removes the path; second line sets it.  Without the first line,
 # your path gets massive and fish becomes very slow.
 set -e fish_user_paths
-set -U fish_user_paths $HOME/.local/bin $HOME/.config/emacs/bin $HOME/Applications $fish_user_paths
+set -U fish_user_paths $HOME/.bin $HOME/.lmstudio/bin $HOME/.local/bin $HOME/AppImages /var/lib/flatpak/exports/bin/ $fish_user_paths
 
 ### EXPORT ###
-
-set fish_greeting
-set TERM 'xterm-256color'
-set EDITOR 'nvim'
-set VISUAL 'nvim'
+set fish_greeting # Supresses fish's intro message
+set TERM xterm-256color # Sets the terminal type
+set EDITOR nvim # use neovim in terminal
+set VISUAL kate # se kate in GUI mode
 
 ### SET FZF DEFAULTS
-
-set FZF_DEFAULT_OPTS '--layout=reverse --exact --border=bold --border=rounded --margin=3% --color=dark'
+set -gx FZF_DEFAULT_OPTS "--layout=reverse --exact --border=bold --border=rounded --margin=3% --color=dark"
 
 ### SET MANPAGER
+### Uncomment only one of these!
 
-# 'nvim' as manpager
-set -x MANPAGER 'nvim +Man!'
+### "nvim" as manpager
+set -x MANPAGER "nvim +Man!"
 
-### SET VI MODE ###
+### "less" as manpager
+# set -x MANPAGER "less"
 
+### SET EITHER DEFAULT EMACS MODE OR VI MODE ###
 function fish_user_key_bindings
-  fish_vi_key_bindings
+    # fish_default_key_bindings
+    fish_vi_key_bindings
 end
+### END OF VI MODE ###
 
-### SET CURSOR ###
-# block, line, underscore
-set fish_cursor_default block
-set fish_cursor_insert underline
+### AUTOCOMPLETE AND HIGHLIGHT COLORS ###
+set fish_color_normal brcyan
+set fish_color_autosuggestion '#7d7d7d'
+set fish_color_command brcyan
+set fish_color_error '#ff6c6b'
+set fish_color_param brcyan
 
 ### FUNCTIONS ###
+
 # Functions needed for !! and !$
 function __history_previous_command
-  switch (commandline -t)
-  case '!'
-    commandline -t $history[1]; commandline -f repaint
-  case '*'
-    commandline -i !
-  end
+    switch (commandline -t)
+        case "!"
+            commandline -t $history[1]
+            commandline -f repaint
+        case "*"
+            commandline -i !
+    end
 end
 
 function __history_previous_command_arguments
-  switch (commandline -t)
-  case '!'
-    commandline -t ''
-    commandline -f history-token-search-backward
-  case '*'
-    commandline -i '$'
-  end
+    switch (commandline -t)
+        case "!"
+            commandline -t ""
+            commandline -f history-token-search-backward
+        case "*"
+            commandline -i '$'
+    end
 end
 
 # The bindings for !! and !$
-if [ '$fish_key_bindings' = 'fish_vi_key_bindings' ];
-  bind -Minsert ! __history_previous_command
-  bind -Minsert '$' __history_previous_command_arguments
+if [ "$fish_key_bindings" = fish_vi_key_bindings ]
+
+    bind -Minsert ! __history_previous_command
+    bind -Minsert '$' __history_previous_command_arguments
 else
-  bind ! __history_previous_command
-  bind '$' __history_previous_command_arguments
+    bind ! __history_previous_command
+    bind '$' __history_previous_command_arguments
+end
+
+# Function for creating a backup file
+# ex: backup file.txt
+# result: copies file as file.txt.bak
+function backup --argument filename
+    cp $filename $filename.bak
 end
 
 # Function for copying files and directories, even recursively.
@@ -77,56 +81,160 @@ end
 # result: copies the directory and all of its contents.
 function copy
     set count (count $argv | tr -d \n)
-    if test '$count' = 2; and test -d '$argv[1]'
-	set from (echo $argv[1] | trim-right /)
-	set to (echo $argv[2])
+    if test "$count" = 2; and test -d "$argv[1]"
+        set from (echo $argv[1] | trim-right /)
+        set to (echo $argv[2])
         command cp -r $from $to
     else
         command cp $argv
     end
 end
 
-### ALIASES ###
+# Function for printing a column (splits input on whitespace)
+# ex: echo 1 2 3 | coln 3
+# output: 3
+function coln
+    while read -l input
+        echo $input | awk '{print $'$argv[1]'}'
+    end
+end
 
+# Function for printing a row
+# ex: seq 3 | rown 3
+# output: 3
+function rown --argument index
+    sed -n "$index p"
+end
+
+# Function for ignoring the first 'n' lines
+# ex: seq 10 | skip 5
+# results: prints everything but the first 5 lines
+function skip --argument n
+    tail +(math 1 + $n)
+end
+
+# Function for taking the first 'n' lines
+# ex: seq 10 | take 5
+# results: prints only the first 5 lines
+function take --argument number
+    head -$number
+end
+
+### CREATE A DIRECTORY & cd INTO ###
+function mkcd
+    mkdir -p $argv[1] && cd $argv[1]
+end
+
+### END OF FUNCTIONS ###
+
+### ALIASES ###
 # navigation
 alias ..='cd ..'
 alias ...='cd ../..'
 alias .3='cd ../../..'
 alias .4='cd ../../../..'
-#alias .5='cd ../../../../..'
+alias .5='cd ../../../../..'
+alias cdg='cd ~/git'
+alias cdgs='cd ~/git/dasharpest'
 
-# file
-alias cdl='cd $1 && ls'
-alias mkdir='mkdir -p'
-alias cdc='cd ~/.config'
-alias cat='bat --theme=base16'
-alias ls='eza --icons=always --color=always -a'
-alias ll='eza --icons=always --color=always -la'
-alias lf='yazi'
+# vim
+alias vim='nvim'
+alias v='nvim'
 
-# system
-alias maintain='yay -Sc && sudo pacman -Scc'
-alias purge='pacman -Qtdq | sudo pacman -Rns -' # remove unused packages, also try > pacman -Qqd | pacman -Rsu --print -
-alias trim='sudo fstrim -av'
-alias update-grub='sudo grub-mkconfig -o /boot/grub/grub.cfg'
-alias umnt='sudo umount -a'
+# Changing "ls" to "eza"
+alias ls='eza -al --color=always --group-directories-first --icons' # my preferred listing
+alias la='eza -a --color=always --group-directories-first --icons' # all files and dirs
+alias ll='eza -l --color=always --group-directories-first --icons' # long format
+alias lt='eza -aT --color=always --group-directories-first' # tree listing
+alias l.='eza -a | egrep "^\."'
+alias l.='eza -al --color=always --group-directories-first ../' # ls on the PARENT directory
+alias l..='eza -al --color=always --group-directories-first ../../' # ls on directory 2 levels up
+alias l...='eza -al --color=always --group-directories-first ../../../' # ls on directory 3 levels up
+
+# pacman and yay
+alias pacsyu='sudo pacman -Syu' # update only standard pkgs
+alias pacsyyu='sudo pacman -Syyu' # Refresh pkglist & update standard pkgs
+alias yaysua='yay -Sua --noconfirm' # update only AUR pkgs (yay)
+alias yaysyu='yay -Syu --noconfirm' # update standard pkgs and AUR pkgs (yay)
+alias pacdb='sudo rm /var/lib/pacman/db.lck' # remove pacman lock
+alias purge='sudo pacman -Rns (pacman -Qtdq)' # remove orphaned packages (DANGEROUS!)
+
+# get fastest mirrors
+alias mirror="sudo reflector -f 30 -l 30 --number 10 --verbose --save /etc/pacman.d/mirrorlist"
+alias mirrord="sudo reflector --latest 50 --number 20 --sort delay --save /etc/pacman.d/mirrorlist"
+alias mirrors="sudo reflector --latest 50 --number 20 --sort score --save /etc/pacman.d/mirrorlist"
+alias mirrora="sudo reflector --latest 50 --number 20 --sort age --save /etc/pacman.d/mirrorlist"
+alias mirroruk="sudo reflector --country 'United Kingdom' --age 12 --protocol https --sort rate --verbose --save /etc/pacman.d/mirrorlist"
+
+# adding flags
+alias df='df -h' # human-readable sizes
+alias free='free -m' # show sizes in MB
+alias grep='grep --color=auto' # colorize output (good for log files)
+
+# ps
+alias psa="ps auxf"
+alias psgrep="ps aux | grep -v grep | grep -i -e VSZ -e"
+alias psmem='ps auxf | sort -nr -k 4'
+alias pscpu='ps auxf | sort -nr -k 3'
+
+# Merge Xresources
+alias merge='xrdb -merge ~/.Xresources'
 
 # git
-alias gg='cd ~/git'
-alias ga='git add'
-alias gc='git commit'
-alias gp='git push'
-alias gpl='git pull'
+alias ga='git add -u'
+alias gaa='git add .'
+alias branch='git branch'
+alias gch='git checkout'
 alias gcl='git clone'
-alias gs='git status'
-alias lg='lazygit'
+alias gc='git commit -m'
+alias gf='git fetch'
+alias gpl='git pull origin'
+alias gp='git push origin'
+alias gt='git tag'
+alias gnt='git tag -a'
 
-# virt machines
-alias vmon='sudo systemctl start libvirtd.service'
-alias vmoff='sudo systemctl stop libvirtd.service'
+# get error messages from journalctl
+alias jctl="journalctl -p 3 -xb"
 
-# user
-alias clpw='cliphist wipe'
-alias ff='fastfetch'
-alias cm='cmatrix -u 6'
+# gpg encryption
+# verify signature for isos
+alias gpg-check="gpg2 --keyserver-options auto-key-retrieve --verify"
+# receive the key of a developer
+alias gpg-retrieve="gpg2 --keyserver-options auto-key-retrieve --receive-keys"
 
+# change your default USER shell
+alias tobash="chsh $USER -s /bin/bash && echo 'Log out and log back in for change to take effect.'"
+alias tozsh="chsh $USER -s /bin/zsh && echo 'Log out and log back in for change to take effect.'"
+alias tofish="chsh $USER -s /bin/fish && echo 'Log out and log back in for change to take effect.'"
+
+# lm studio
+alias lmsdu="lms daemon up"
+alias lmsdd="lms daemon down"
+
+# programs
+alias ff="fastfetch"
+alias opc="opencode"
+
+# Mocp must be launched with bash instead of Fish!
+alias mocp="bash -c mocp"
+
+### RANDOM COLOR SCRIPT ###
+# Get this script from my GitLab: gitlab.com/dwt1/shell-color-scripts
+# Or install it from the Arch User Repository: shell-color-scripts
+# The 'if' statement prevents colorscript from showing in 'fzf' previews.
+#if status is-interactive
+#    colorscript random
+#end
+
+### SETTING THE STARSHIP PROMPT ###
+starship init fish | source
+
+### FZF ###
+# Enables the following keybindings:
+# CTRL-t = fzf select
+# CTRL-r = fzf history
+# ALT-c  = fzf cd
+fzf --fish | source
+
+# Added by LM Studio CLI tool (lms)
+set -gx PATH $PATH /home/sharpest/.lmstudio/bin
